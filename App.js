95a344451824f698756b1beeb46faf0067792f49
shuklavishwan <vishwam.shukla@spare-it.com>
2024-08-14 03:09:18 +0530

    Added fonts

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import LoginScreen from './Apps/Screens/LoginScreen/LoginScreen';
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    'outfit-regular': require('./assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium': require('./assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start wor!</Text>
-      <StatusBar style="auto" />
+        <LoginScreen/>
     </View>
   );
 }
@@ -14,7 +21,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
